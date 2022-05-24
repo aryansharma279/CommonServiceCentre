f3f0feaa34088b7824d2ab34c9add33da902ee73
Tushar Kumar <tusharkumar@Tushars-MacBook-Pro.local>
2022-05-24 18:10:06 +0530

    [NEW] added the new changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,85 +1,132 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
 
 import * as firebase from 'firebase';
 
 
-@Injectable({
-    providedIn: 'root',
-  })
-  export class DataService {
+@Injectable({providedIn: 'root'})
+export class DataService {
     private dbPath = 'digitalseva';
-    dbRef: firebase.default.database.Database;
-  
-    constructor(private db: AngularFireDatabase) {
-      this.dbRef = this.db.database;
+    dbRef : firebase.default.database.Database;
+
+    constructor(private db : AngularFireDatabase) {
+        this.dbRef = this.db.database;
     }
     createService(payload) {
-        return this.dbRef
-          .ref(`${this.dbPath}/services/${payload.name}`)
-       
-          .set(payload, (error) => {
-            if (error) {
-              // The write failed...
-              console.log('error while adding the data', error);
-              return false;
-            } else {
-              // Data saved successfully!
-              console.log('Data Uploaded !');
-              return true;
+        return this.dbRef.ref(`${
+            this.dbPath
+        }/services/${
+            payload.name
+        }`).set(payload, (error) => {
+            if (error) { // The write failed...
+                console.log('error while adding the data', error);
+                return false;
+            } else { // Data saved successfully!
+                console.log('Data Uploaded !');
+                return true;
             }
-          });
-      }
-       // read
-  getAllServices() {
-    return this.dbRef.ref(`${this.dbPath}/services`);
-  }
+        });
+    }
+    // read
+    getAllServices() {
+        return this.dbRef.ref(`${
+            this.dbPath
+        }/services`);
+    }
     // update
     editService(payload) {
-      // Get a key for a new Post.
-      // var newPostKey = this.dbRef.ref(`${this.dbPath}/games/${gametype}`).child(`${payload.name}`).push().key;
-  
-      // Write the new post's data simultaneously in the posts list and the user's post list.
-      var updates = {};
-      updates[`${this.dbPath}/services/${payload.name}/name`] =
-        payload.name;
-      updates[`${this.dbPath}/services/${payload.name}/description`] =
-        payload.description;
-        updates[`${this.dbPath}/services/${payload.name}/documents`] =
-        payload.documents;
-        updates[`${this.dbPath}/services/${payload.name}/charges`] =
-        payload.charges;
-      // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-  
-      return this.dbRef.ref().update(updates);
+        // Get a key for a new Post.
+        // var newPostKey = this.dbRef.ref(`${this.dbPath}/games/${gametype}`).child(`${payload.name}`).push().key;
+
+        // Write the new post's data simultaneously in the posts list and the user's post list.
+        var updates = {};
+        updates[`${
+                this.dbPath
+            }/services/${
+                payload.name
+            }/name`] = payload.name;
+        updates[`${
+                this.dbPath
+            }/services/${
+                payload.name
+            }/description`] = payload.description;
+        updates[`${
+                this.dbPath
+            }/services/${
+                payload.name
+            }/documents`] = payload.documents;
+        updates[`${
+                this.dbPath
+            }/services/${
+                payload.name
+            }/charges`] = payload.charges;
+        // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+        return this.dbRef.ref().update(updates);
     }
-  
 
-  deleteService(name) {
-    return this.dbRef.ref(`${this.dbPath}/services/${name}`).remove();
-  }
-  extractServices(snapshot) {
-    const services = [];
-    console.log('snapshot ', snapshot.val());
 
-    // as the data is inside the format of object convert that into array to iterate
-    const data = snapshot.val();
-
-    if (data) {
-      // keys
-      const serviceKeys = Object.keys(data);
-      serviceKeys.forEach((service) => {
-        services.push(data[service]);
-      });
-
-      console.log('Services in the Services array', services);
-
-      console.log('data of the all Services', data);
-
-      // all the Services will be converted to the array
-      return services;
-    } else {
-      return [];
+    deleteService(name) {
+        return this.dbRef.ref(`${
+            this.dbPath
+        }/services/${name}`).remove();
     }
-  }
+    extractServices(snapshot) {
+        const services = [];
+        console.log('snapshot ', snapshot.val());
+
+        // as the data is inside the format of object convert that into array to iterate
+        const data = snapshot.val();
+
+        if (data) { // keys
+            const serviceKeys = Object.keys(data);
+            serviceKeys.forEach((service) => {
+                services.push(data[service]);
+            });
+
+            console.log('Services in the Services array', services);
+
+            console.log('data of the all Services', data);
+
+            // all the Services will be converted to the array
+            return services;
+        } else {
+            return [];
+        }
     }
+
+
+    getAdminBookings() {
+        return this.dbRef.ref(`${
+            this.dbPath
+        }/bookings`);
+
+
+    }
+
+    getBookingOfUser(email) {
+        const emailSplitted = email.split('@')[0];
+        return this.dbRef.ref(`${
+            this.dbPath
+        }/users/${emailSplitted}/bookings`);
+    }
+
+
+    updateAdminBookings(bookings) {
+        var updates = {};
+        updates[`${
+                this.dbPath
+            }/bookings`] = bookings;
+
+        return this.dbRef.ref().update(updates);
+    }
+    updateUserBookings(bookings,email) {
+      const emailSplitted = email.split('@')[0];
+        var updates = {};
+        updates[`${
+          this.dbPath
+      }/users/${emailSplitted}/bookings`] = bookings;
+
+        return this.dbRef.ref().update(updates);
+    }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DataService} from '../services/data.service';
 
-@Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
-})
+@Component({selector: 'app-tab2', templateUrl: 'tab2.page.html', styleUrls: ['tab2.page.scss']})
 export class Tab2Page {
 
-  constructor() {}
+    statusList = ['pending', 'processing', 'completed', 'delivered']
+
+    bookings : any;
+    userBookings : any;
+
+    constructor(private dataService : DataService) {}
+
+    ngOnInit(): void {
+        this.getBookings();
+
+
+    }
+
+
+    getBookings() {
+
+        this.dataService.getAdminBookings().on('value', (snapshot) => {
+            console.log('snapshot', snapshot.val());
+
+            this.bookings = snapshot.val() || [];
+        })
+
+    }
+
+    statusChanged(event, booking, index) {
+        console.log('event', event.detail.value);
+        console.log('booking', booking);
+        this.bookings[index]['status'] = event.detail.value;
+
+        console.log('bookings', this.bookings);
+
+
+        this.updateAdminBookings(booking.email);
+        this.updateUserBooking(booking, event.detail.value);
+        return;
+
+
+    }
+
+
+    updateAdminBookings(email) {
+        this.dataService.updateAdminBookings(this.bookings).then((response) => {
+            console.log('reponse from updating bookings for admin', response);
+
+
+        }).catch((error) => {
+            console.log('error', error);
+        })
+    }
+
+
+    updateUserBooking(booking, status) { // this.dataService
+        this.dataService.getBookingOfUser(booking.email).on('value', (snapshot) => {
+            console.log('snapshot', snapshot.val());
+            this.userBookings = snapshot.val();
+
+
+        })
+
+
+        if (this.userBookings) {
+            this.userBookings.forEach((item) => {
+                if (item.name === booking.name) {
+                    item.status = status;
+                }
+            })
+
+            console.log('user bookings', this.userBookings);
+
+            this.dataService.updateUserBookings(this.userBookings, booking.email).then((response) => {
+                console.log('reponse from updating bookings for user', response);
+
+            }).catch((error) => {
+                console.log('error', error);
+            })
+        }
+
+
+    }
 
 }
